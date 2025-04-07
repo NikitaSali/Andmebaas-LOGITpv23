4d25ea5dab48d230381358f155738e367244ad7b
NikitaSali <nikitasali30@gmail.com>
2025-04-07 11:04:31 +0300

    tabeli loomine SQL laused

--- SQL laused.sql
+++ SQL laused.sql
@@ -1,1 +1,24 @@
+--kommentaar
+--SQL SERVER Managment Stuudio
+--Connect TO:
+--Server Name: (localdb)\mssqllocaldb
+--Authentification 2 tüüpi:
+--1. Windows Auth - localdb admini õigused
+--2. SQL Server Auth - kontrollida varem tehtud kasutajad
+CREATE DATABASE saliAndmebaas;
+USE saliAndmebaas;
 
+--tabeli loomine
+--identity(1,1) - ise täidab tabeli 1,2,3,...
+CREATE TABLE inimene(
+inimeneID int Primary Key identity(1,1),
+nimi varchar(50) unique,
+sünniaeg date,
+telefon char(12),
+pikkus decimal(5,2),
+õpilaskodu bit
+);
+SELECT * FROM inimene;
+
+--tabeli kustutamine
+DROP table inimene;
