11c3a568ac2afc820afec68226fe8776d7e1d660
NikitaSali <nikitasali30@gmail.com>
2025-04-08 14:43:40 +0300

    Xampp

---
+++ saliandmebaas.sql
@@ -0,0 +1,231 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Loomise aeg: Aprill 08, 2025 kell 01:42 PL
+-- Serveri versioon: 10.4.32-MariaDB
+-- PHP versioon: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Andmebaas: `saliandmebaas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `auto`
+--
+
+CREATE TABLE `auto` (
+  `autoID` int(11) NOT NULL,
+  `autoNr` varchar(50) DEFAULT NULL,
+  `mudell` varchar(50) DEFAULT NULL,
+  `mark` varchar(50) DEFAULT NULL,
+  `aasta` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `auto`
+--
+
+INSERT INTO `auto` (`autoID`, `autoNr`, `mudell`, `mark`, `aasta`) VALUES
+(1, 'turbos', 'tunning', 'porche', '2012-03-05');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `elukoht`
+--
+
+CREATE TABLE `elukoht` (
+  `elukohtID` int(11) NOT NULL,
+  `elukoht` varchar(50) DEFAULT NULL,
+  `maakond` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `elukoht`
+--
+
+INSERT INTO `elukoht` (`elukohtID`, `elukoht`, `maakond`) VALUES
+(1, 'Tartu', 'Tartumaa'),
+(2, 'Pärnu', 'Pärnumaa');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `inimene`
+--
+
+CREATE TABLE `inimene` (
+  `inimeneID` int(11) NOT NULL,
+  `nimi` varchar(50) DEFAULT NULL,
+  `sünniaeg` date DEFAULT NULL,
+  `telefon` char(12) DEFAULT NULL,
+  `pikkus` decimal(5,2) DEFAULT NULL,
+  `õpilaskodu` bit(1) DEFAULT NULL,
+  `elukohtID` int(11) DEFAULT NULL,
+  `autoID` int(11) DEFAULT NULL,
+  `loomID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `inimene`
+--
+
+INSERT INTO `inimene` (`inimeneID`, `nimi`, `sünniaeg`, `telefon`, `pikkus`, `õpilaskodu`, `elukohtID`, `autoID`, `loomID`) VALUES
+(1, 'Peeter Uus', '2021-12-30', '2568952', 90.50, b'0', NULL, NULL, NULL),
+(2, 'Kaarel Oja', '2000-12-30', '2568952', 90.50, b'0', NULL, NULL, NULL),
+(3, 'Kask Mati', '1983-12-30', '333333', 140.50, b'0', NULL, NULL, NULL),
+(4, 'Peeter Oja', '2008-12-30', '3333333', 190.50, b'0', NULL, NULL, NULL),
+(5, 'Peeter Vana', '2021-12-30', '2568952', 90.50, b'0', 1, NULL, NULL),
+(6, 'turbo2', '2001-12-30', '091236543', 10.00, b'0', 1, 1, NULL),
+(10, 'Pavel', '2001-12-30', '091236543', 10.00, b'0', 1, NULL, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `loom`
+--
+
+CREATE TABLE `loom` (
+  `loomID` int(11) NOT NULL,
+  `nimi` varchar(50) DEFAULT NULL,
+  `kaal` decimal(10,2) DEFAULT NULL,
+  `tyypID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `loom`
+--
+
+INSERT INTO `loom` (`loomID`, `nimi`, `kaal`, `tyypID`) VALUES
+(2, 'kass', 3.00, NULL),
+(3, 'kass', 3.00, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `tyyp`
+--
+
+CREATE TABLE `tyyp` (
+  `tyypID` int(11) NOT NULL,
+  `tyyp` varchar(50) DEFAULT NULL,
+  `kirjeldus` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `tyyp`
+--
+
+INSERT INTO `tyyp` (`tyypID`, `tyyp`, `kirjeldus`) VALUES
+(1, 'Башкирский', 'оранжевый в полосочку');
+
+--
+-- Indeksid tõmmistatud tabelitele
+--
+
+--
+-- Indeksid tabelile `auto`
+--
+ALTER TABLE `auto`
+  ADD PRIMARY KEY (`autoID`);
+
+--
+-- Indeksid tabelile `elukoht`
+--
+ALTER TABLE `elukoht`
+  ADD PRIMARY KEY (`elukohtID`),
+  ADD UNIQUE KEY `elukoht` (`elukoht`);
+
+--
+-- Indeksid tabelile `inimene`
+--
+ALTER TABLE `inimene`
+  ADD PRIMARY KEY (`inimeneID`),
+  ADD UNIQUE KEY `nimi` (`nimi`),
+  ADD KEY `fk_elukoht` (`elukohtID`),
+  ADD KEY `fk_autoID` (`autoID`),
+  ADD KEY `fk_loom` (`loomID`);
+
+--
+-- Indeksid tabelile `loom`
+--
+ALTER TABLE `loom`
+  ADD PRIMARY KEY (`loomID`),
+  ADD KEY `fk_tyyp` (`tyypID`);
+
+--
+-- Indeksid tabelile `tyyp`
+--
+ALTER TABLE `tyyp`
+  ADD PRIMARY KEY (`tyypID`);
+
+--
+-- AUTO_INCREMENT tõmmistatud tabelitele
+--
+
+--
+-- AUTO_INCREMENT tabelile `auto`
+--
+ALTER TABLE `auto`
+  MODIFY `autoID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT tabelile `elukoht`
+--
+ALTER TABLE `elukoht`
+  MODIFY `elukohtID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT tabelile `inimene`
+--
+ALTER TABLE `inimene`
+  MODIFY `inimeneID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT tabelile `loom`
+--
+ALTER TABLE `loom`
+  MODIFY `loomID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT tabelile `tyyp`
+--
+ALTER TABLE `tyyp`
+  MODIFY `tyypID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Tõmmistatud tabelite piirangud
+--
+
+--
+-- Piirangud tabelile `inimene`
+--
+ALTER TABLE `inimene`
+  ADD CONSTRAINT `fk_autoID` FOREIGN KEY (`autoID`) REFERENCES `auto` (`autoID`),
+  ADD CONSTRAINT `fk_elukoht` FOREIGN KEY (`elukohtID`) REFERENCES `elukoht` (`elukohtID`),
+  ADD CONSTRAINT `fk_loom` FOREIGN KEY (`loomID`) REFERENCES `loom` (`loomID`);
+
+--
+-- Piirangud tabelile `loom`
+--
+ALTER TABLE `loom`
+  ADD CONSTRAINT `fk_tyyp` FOREIGN KEY (`tyypID`) REFERENCES `tyyp` (`tyypID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
